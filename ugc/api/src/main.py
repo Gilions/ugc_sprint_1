e2477bf13fe571333f5adb454c13ba7e7543a722
vitalijsofronuk <v.sofronyuk@puls.ru>
2022-07-27 20:15:10 +0300

    refactoring: remove functions create_topic

--- ugc/api/src/main.py
+++ ugc/api/src/main.py
@@ -4,23 +4,16 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from api.v1 import kafka_producer
-from service.kafka_setter import create_topic
 
 
 logger = logging.getLogger(__name__)
 
-app = FastAPI(title="UGC",
+app = FastAPI(
+    title="UGC",
     description='Асинхронный сборщик UGC',
     docs_url='/api/openapi',
     openapi_url='/api/openapi.json',
     default_response_class=ORJSONResponse,)
 
 
-@app.on_event("startup")
-def startup_event():
-    # create_topic()
-    pass
-
-
 app.include_router(kafka_producer.router, prefix='/api/v1/kafka')
-
